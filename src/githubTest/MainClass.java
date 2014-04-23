842614364e8c3402ae0ca6384189d97da29cecf7
Martijn <mfrielin@student.avans.nl>
2014-04-23 16:13:07 +0200

    more changes to Main

--- src/githubTest/MainClass.java
+++ src/githubTest/MainClass.java
@@ -5,6 +5,8 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, this is a test");
+		
+		System.out.println("Toevoeging!.. more code");
 	}
 
 }
